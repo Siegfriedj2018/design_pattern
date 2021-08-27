d5be83434bba9d8f13787ede584ef161ded053ab
Siegfriedj2020 <justasiegfried09@gmail.com>
2021-08-27 15:32:49 -0400

    added ListDriver and just got started on Guestlist

---
+++ strategy/GuestList.java
@@ -0,0 +1,12 @@
+package strategy;
+
+import java.util.ArrayList;
+
+public class GuestList {
+    protected String title;
+    protected ArrayList<String> people;
+
+    public GuestList(String title) {
+
+    }
+}
