0ef5127813773dfd5f1b324972dbbdcca11eeff8
Siegfriedj2020 <justasiegfried09@gmail.com>
2021-09-30 04:44:43 -0400

    Completed Question.java to create question object

--- singleton/Question.java
+++ singleton/Question.java
@@ -1,4 +1,37 @@
-
 public class Question {
-    
+    private String question;
+    private String[] answers;
+    private int correctAnswer;
+
+    public Question(String question, 
+                    String ans1, 
+                    String ans2, 
+                    String ans3, 
+                    String ans4, 
+                    int correctAnswer) {
+        this.question = question;
+        answers = new String[4];
+        answers[0] = ans1;
+        answers[1] = ans2;
+        answers[2] = ans3;
+        answers[3] = ans4;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String toString() {
+        String result = question + "\n";
+        for (int i = 0; i < 4; i++) {
+            result += (i+1) + ". " + answers[i] + "\n";
+        }
+        result += "The correct answer is: " + getCorrectAnswer() + "\n";
+        return result;
+    }
+
+    public boolean isCorrect(int userAnswer) {
+        return userAnswer == correctAnswer + 1;
+    }
+
+    public String getCorrectAnswer() {
+        return answers[correctAnswer];
+    }
 }
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -1,4 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class TriviaGame {
+    private TriviaGame triviaGame;
+    private int score;
+    private Random rand;
+    private Scanner reader;
+    
     
 }
