71bf06c135b67408b1682de8c33a8212e8115bc4
Siegfriedj2020 <justasiegfried09@gmail.com>
2021-10-02 17:17:27 -0400

    Added javaDoc, output.png, and fixed output spacing

--- singleton/Question.java
+++ singleton/Question.java
@@ -1,8 +1,23 @@
+/**
+ * The Question class creates a question instance and 
+ * it can check if the user entered the correct answer,
+ * or get the correct answer to the question.
+ */
 public class Question {
     private String question;
     private String[] answers;
     private int correctAnswer;
 
+    /**
+     * The question constructor creates a question object and assigns 
+     * the appropriate variables.
+     * @param question The question to store in array.
+     * @param ans1 The first answer to question.
+     * @param ans2 The second answer to question.
+     * @param ans3 The third answer to question.
+     * @param ans4 The fourth answer to question.
+     * @param correctAnswer The correct answer that is zero-based.
+     */
     public Question(String question, String ans1, String ans2, String ans3, String ans4, int correctAnswer) {
         this.question = question;
         answers = new String[4];
@@ -13,19 +28,34 @@ public class Question {
         this.correctAnswer = correctAnswer;
     }
 
+    /**
+     * The toString method returns a string representation of the 
+     * the question and the answers.
+     */
     public String toString() {
         String result = question + "\n";
         for (int i = 0; i < 4; ++i) {
             result += (i + 1) + ". " + answers[i] + "\n";
         }
-        
+
         return result;
     }
 
+    /**
+     * The isCorrect method compares the user answer to the correct answer
+     * that is stored and returns true or false. correctAnswer is zero-based 
+     * so one is added to it for comparison.
+     * @param userAnswer The user input.
+     * @return Returns true if correct, false otherwise.
+     */
     public boolean isCorrect(int userAnswer) {
         return userAnswer == correctAnswer + 1;
     }
 
+    /**
+     * The getCorrectAnswer gets the correct answer from the array of answers.
+     * @return Returns the correct answer from array using correctAnswer variable.
+     */
     public String getCorrectAnswer() {
         return answers[correctAnswer];
     }
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -2,6 +2,10 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ * The TriviaGame class creates a trivia game by calling getinstance
+ * and it calls the private constructor.
+ */
 public class TriviaGame {
     private static TriviaGame triviaGame;
     private int score;
@@ -9,14 +13,26 @@ public class TriviaGame {
     private Scanner reader;
     private ArrayList<Question> questions;
 
+    /**
+     * The TriviaGame constructor creates and instance of triviagame and 
+     * instaniates all the needed variables and it creates an arraylist of 
+     * questions by calling the dataloader which will read in questions and
+     * add them to a question object, then it welcomes the user.
+     */
     private TriviaGame() {
         score = 0;
         rand = new Random();
         reader = new Scanner(System.in);
         questions = DataLoader.getTriviaQuestions();
-        System.out.println("Welcome, user to Trivia Game.");
+        System.out.println("Welcome to Trivia Game.");
     }
 
+    /**
+     * The getInstance method will check to see if there is already an 
+     * instance of Triviagame already instaniated and if not it will create
+     * one or it just returns the already active instance.
+     * @return Returns a TriviaGame instance
+     */
     public static TriviaGame getInstance() {
         if (triviaGame == null) {
             triviaGame = new TriviaGame();
@@ -25,6 +41,14 @@ public class TriviaGame {
         return triviaGame;
     }
 
+    /**
+     * The play method will start the triviagame by calling playaround 
+     * method and if true is returned it will increment score. 
+     * Then the system will ask the user if they want to play again or quit
+     * and it validates the answer if invalid character is provide they are ask
+     * to try again. If valid input is provide the user is told how manyy games
+     * they won and says goodbye.
+     */
     public void play() {
         String play = "";
         boolean isFinished = false;
@@ -55,14 +79,21 @@ public class TriviaGame {
            
         }
         reader.close();
-        System.out.println("You won " + score + " games!");
+        System.out.println("\nYou won " + score + " games!");
         System.out.println("Goodbye!");
     }
 
+    /**
+     * The playround method gets a random question from the arraylist
+     * and displays it and then prompts the user for an answer. It
+     * then checks the users answer with the correct one and returns 
+     * true or false
+     * @return Returns true if correct answer provide, false otherwise.
+     */
     private boolean playRound() {
-        int questionNumber = rand.nextInt(questions.size());
+        int questionNumber = rand.nextInt(questions.size() + 1);
         Question currentQuestion = questions.get(questionNumber);
-        System.out.println(currentQuestion);
+        System.out.println("\n" + currentQuestion);
         System.out.print("Enter Answer: ");
 
         int userAnswer = 0;
@@ -70,17 +101,18 @@ public class TriviaGame {
             userAnswer = reader.nextInt();
             reader.nextLine(); // I'm just ignoring what happens here to advance cursor.
         } catch (Exception e) {
-            System.out.println("Please enter a number 1-4.");
+            System.out.println("Please enter a number 1-4.\n");
             reader.nextLine(); // I'm just ignoring what happens with incorrect input to advance cursor.
         }
 
         if (!currentQuestion.isCorrect(userAnswer)) {
             System.out.println("You got it wrong!");
-            System.out.println("The correct answer is " + currentQuestion.getCorrectAnswer());
+            System.out.println("The correct answer is " + 
+                                currentQuestion.getCorrectAnswer());
             return false;
         }
 
-        System.out.println("YAY! You got it right!");
+        System.out.println("YAY! You got it right!\n");
         return true;
     }
 }
