5ca8a338643938f88caef2e4aead9c7c745d1e9e
Siegfriedj2020 <justasiegfried09@gmail.com>
2021-11-19 17:02:57 -0500

    Finished easy, medium, and hard java files

--- state/Hard.java
+++ state/Hard.java
@@ -6,20 +6,24 @@ public class Hard implements State {
         this.game = game;
     }
 
+    @Override
     public int getNum() {
         return randomNum(1, 100);
     }
 
+    @Override
     public String getOperation() {
         String[] hardOp = {"+", "-", "*", "/"};
         return hardOp[randomNum(0, 4)];
 
     }
 
+    @Override
     public void levelUp() {
         System.out.println("You are doing so well!!!");
     }
 
+    @Override
     public void levelDown() {
         System.out.println("You are struggling, let's go to medium mode.");
         game.setState(game.getMediumState);
--- state/Medium.java
+++ state/Medium.java
@@ -6,20 +6,24 @@ public class Medium {
         this.game = game;
     }
     
+    @Override
     public int getNum() {
         return randomNum(1, 50);
     }
 
+    @Override
     public String getOperation() {
         String[] mediumOp = {"+", "-", "*"};
         return mediumOp[randomNum(0, 3)];
     }
 
+    @Override
     public void levelUp() {
         System.out.println("You've been advanced to the hardest mode.");
         game.setState(game.getHardState());
     }
 
+    @Override
     public void levelDown() {
         System.out.println("You are struggling, let's go to easy mode.");
         game.setState(game.getEasyState());
